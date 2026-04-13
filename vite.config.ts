778063b4cc7fc49a82d8a66b74f7c2c8529fcd08
sagarhasan273 <sagarhasan273@gmail.com>
2026-04-13 18:38:54 +0600

    Fix: early slash before src is removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'portfolio',
+  base: '/',   // ✅ FIX
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
