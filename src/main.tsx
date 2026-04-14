e6f8c182e1367ee6a94c7f02d4fd37bf665016fc
sagarhasan273 <sagarhasan273@gmail.com>
2026-04-15 00:36:23 +0600

    update: Added CSS to scroll bar

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'; // 👈 MUST HAVE THIS
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
